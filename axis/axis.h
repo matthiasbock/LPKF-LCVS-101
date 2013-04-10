d03babe6e31612a1943dea8bf5c2e857d587bbe4
Matthias <mail@matthiasbock.net>
2013-04-10 17:19:21 +0200

    pins

--- axis/axis.h
+++
@@ -1,9 +0,0 @@
-typedef struct L298 {
-  int ENA;
-  int IN1;
-  int IN2;
-  int IN3;
-  int IN4;
-  int ENB;
-} L298;
-
--- axis/axis.ino
+++ axis/axis.ino
@@ -2,16 +2,40 @@
 // Arduino driver for Keyes L298 motor module/shield
 //
 
-#include </home/code/Arduino-CNC/axis/axis.h>
+int A_ENA = 28;
+int A_IN1 = 29;
+int A_IN2 = 26;
+int A_IN3 = 27;
+int A_IN4 = 24;
+int A_ENB = 25;
 
-L298 E;
+int B_ENA = 30;
+int B_IN1 = 31;
+int B_IN2 = 32;
+int B_IN3 = 33;
+int B_IN4 = 34;
+int B_ENB = 35;
 
-int E.ENA=53;
-/*E.IN1 = 52;
-E.IN2 = 51;
-E.IN3 = 50;
-E.IN4 = 49;
-E.ENB = 48;*/
+int C_ENA = 41;
+int C_IN1 = 40;
+int C_IN2 = 39;
+int C_IN3 = 38;
+int C_IN4 = 37;
+int C_ENB = 36;
+
+int D_ENA = 47;
+int D_IN1 = 46;
+int D_IN2 = 45;
+int D_IN3 = 44;
+int D_IN4 = 43;
+int D_ENB = 42;
+
+int E_ENA = 53;
+int E_IN1 = 52;
+int E_IN2 = 51;
+int E_IN3 = 50;
+int E_IN4 = 49;
+int E_ENB = 48;
 
 void init(int pin) {
   pinMode(pin, OUTPUT);
@@ -55,5 +79,6 @@ void TestModule() {
   }
 
 void loop() {
+
   TestModule();
   }
