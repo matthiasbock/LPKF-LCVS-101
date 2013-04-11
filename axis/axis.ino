f953fc556a89a15142ecf7fc6b9ac26a8d33e060
Matthias <mail@matthiasbock.net>
2013-04-11 22:30:35 +0200

    experimental advance

--- axis/axis.ino
+++ axis/axis.ino
@@ -1,21 +1,42 @@
 //
-// Arduino driver for Keyes L298 motor module/shield
+// Arduino driver for LPKF 101 LC/VS milling machine X/Y axis
+// using Keyes L298N motor modules/shields
 //
 
-int ENA = 0;
-int IN1 = 1;
-int IN2 = 2;
-int IN3 = 3;
-int IN4 = 4;
-int ENB = 5;
+      int ENA = 0;
+        int IN1 = 1;
+          int IN2 = 2;
+            int IN3 = 3;
+              int IN4 = 4;
+                int ENB = 5;
 
+// pins at which the five L298 modules are connected to the Arduino
 int A[6] = {47,46,45,44,43,42};
 int B[6] = {53,52,51,50,49,48};
-int E[6] = {28,29,26,27,24,25};
-int D[6] = {30,31,32,33,34,35};
 int C[6] = {41,40,39,38,37,36};
+int D[6] = {30,31,32,33,34,35};
+int E[6] = {28,29,26,27,24,25};
+
+void initPin(int pin) {
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+}
+
+void initModule(int m[6]) {
+  for (int i=0; i<6; i++) initPin(m[i]);
+}
+
+void setup() {
+  initModule(A);
+  initModule(B);
+  initModule(C);
+  initModule(D);
+  initModule(E);
+}
 
 /*
+Y axis stepper motor:
+
 yellow  = A_MotorA1;
 white   = A_MotorA2;
 blue    = A_MotorB1;
@@ -26,37 +47,37 @@ gray    = B_MotorB1;
 black   = B_MotorB2;
 brown   = C_MotorA1;
 violet  = C_MotorA2;
+
+
+X axis stepper motor:
+
+yellow  = C_MotorB1;
+white   = C_MotorB2;
+blue    = D_MotorA1;
+red     = D_MotorA2;
+orange  = D_MotorB1;
+green   = D_MotorB2;
+gray    = E_MotorA1;
+black   = E_MotorA2;
+brown   = E_MotorB1;
+violet  = E_MotorB2;
 */
-
-void init(int pin) {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
-}
-
-void initM(int m[6]) {
-  for (int i=0; i<6; i++) init(m[i]);
-}
-
-void setup() {
-  initM(A);
-  initM(B);
-  initM(C);
-  initM(D);
-  initM(E);
-}
 
 int cyclePos = 1;
 
-void setW1() {
-  int enable = A[ENA];
-  int in1 = A[IN1];
-  int in2 = A[IN2];
-  if ((cyclePos == 2) || (cyclePos == 12)) { // disable
+/*
+  A five phase stepper motor consists of five coils W1 to W5.
+  The following three functions implement the half-step timing procedure
+  found in Berger-Lahr's 5-phase stepping motor datasheet.
+*/
+
+void setWlow(int in1, int in2, int lowDisable, int highDisable) {
+  if ((cyclePos == lowDisable) || (cyclePos == highDisable)) { // disable
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
   }
-  else if ((cyclePos > 2) && (cyclePos < 12)) { // A low, B high
-    // switch the one off before switching on the other
+  else if ((cyclePos > lowDisable) && (cyclePos < highDisable)) { // A low, B high
+    // switch the one off before switching the other on
     digitalWrite(in2, LOW);
     digitalWrite(in1, HIGH);
   }
@@ -66,15 +87,12 @@ void setW1() {
   }
 }
 
-void setW2() {
-  int enable = A[ENB];
-  int in3 = A[IN3];
-  int in4 = A[IN4];
-  if ((cyclePos == 4) || (cyclePos == 14)) { // disable
+void setWhigh(int in3, int in4, int lowDisable, int highDisable) {
+  if ((cyclePos == lowDisable) || (cyclePos == highDisable)) { // disable
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
   }
-  else if ((cyclePos > 4) && (cyclePos < 14)) { // C high, D low
+  else if ((cyclePos > lowDisable) && (cyclePos < highDisable)) { // C high, D low
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
   }
@@ -84,112 +102,54 @@ void setW2() {
   }
 }
 
-void setW3() {
-  int enable = B[ENA];
-  int in1 = B[IN1];
-  int in2 = B[IN2];
-  if ((cyclePos == 6) || (cyclePos == 16)) { // disable
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-  }
-  else if ((cyclePos > 6) && (cyclePos < 16)) { // E low, F high
-    digitalWrite(in2, LOW);
-    digitalWrite(in1, HIGH);
-  }
-  else { // E high, F low
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-  }
+void updateYsWs() {
+  setWlow(  A[IN1], A[IN2], 2, 12 );
+  setWhigh( A[IN3], A[IN4], 4, 14 );
+  setWlow(  B[IN1], B[IN2], 6, 16 );
+  setWhigh( B[IN3], B[IN4], 8, 18 );
+  setWlow(  C[IN1], C[IN2], 10, 20 );  
 }
 
-void setW4() {
-  int enable = B[ENB];
-  int in3 = B[IN3];
-  int in4 = B[IN4];
-  if ((cyclePos == 8) || (cyclePos == 18)) { // disable
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-  }
-  else if ((cyclePos > 8) && (cyclePos < 18)) { // G high, H low
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-  else { // G low, H high
-    digitalWrite(in4, LOW);
-    digitalWrite(in3, HIGH);
-  }
+// enable power for Y axis motor control pins
+void switchYon() {
+  digitalWrite(A[ENA], HIGH);
+  digitalWrite(A[ENB], HIGH);
+  digitalWrite(B[ENA], HIGH);
+  digitalWrite(B[ENB], HIGH);
+  digitalWrite(C[ENA], HIGH);
 }
 
-void setW5() {
-  int enable = C[ENA];
-  int in1 = C[IN1];
-  int in2 = C[IN2];
-  if ((cyclePos == 10) || (cyclePos == 20)) { // disable
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-  }
-  else if ((cyclePos > 10) && (cyclePos < 20)) { // J low, K high
-    digitalWrite(in2, LOW);
-    digitalWrite(in1, HIGH);
-  }
-  else { // high
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-  }
-}
-
-void writeEN(int level) {
-  digitalWrite(A[ENA], level);
-  digitalWrite(A[ENB], level);
-  digitalWrite(B[ENA], level);
-  digitalWrite(B[ENB], level);
-  digitalWrite(C[ENA], level);
-}
-
-void switchOn() {
-  writeEN(HIGH);
-}
-
-void switchOff() {
-  //writeEN(LOW);
-  // switch all LEDs off to show inactivity
-  initM(A);
-  initM(B);
+// disable power for Y axis motor control pins
+void switchYoff() {
+  // also switch all LEDs off to show shutdown
+  initModule(A);
+  initModule(B);
   digitalWrite(C[ENA], LOW);
   digitalWrite(C[IN1], LOW);
   digitalWrite(C[IN2], LOW);
 }
 
+// drive a little forth and back
 void loop() {
-  switchOn();
+  switchYon();
 
   for (int i=0; i<4000; i++) {
-    setW1();
-    setW2();
-    setW3();
-    setW4();
-    setW5();
-    // 5000 steps/s
+    updateYsWs();
     delayMicroseconds(500);
     cyclePos = (cyclePos % 20) + 1;
   }
 
-  switchOff();
+  switchYoff();
   delay(1000);
-  switchOn();
+  switchYon();
 
   for (int i=0; i<4000; i++) {
-    setW1();
-    setW2();
-    setW3();
-    setW4();
-    setW5();
-    // 5000 steps/s
+    updateYsWs();
     delayMicroseconds(500);
     cyclePos = (cyclePos + 19) % 20;
   }
   
-  switchOff();
+  switchYoff();
   delay(1000);
 }
 
