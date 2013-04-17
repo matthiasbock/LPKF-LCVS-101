5e3fc20dfe294c26a219ccf176bfe7afdcc3aec4
Matthias <mail@matthiasbock.net>
2013-04-17 13:33:21 +0200

    spindle driver works

--- spindle/spindle.ino
+++ spindle/spindle.ino
@@ -4,15 +4,21 @@
 // Alfred Jager 33-1 W02
 //
 
+// pins
 #define U_plus 31
 #define U_minus 33
-
 #define V_plus 37
 #define V_minus 39
-
 #define W_plus 43
 #define W_minus 45
-//int forward[3][2] = { {U_plus, V_minus}, {V_plus, W_minus}, {W_plus, U_minus} };
+
+#define U 0
+#define V 1
+#define W 2
+#define plus 0
+#define minus 1
+
+int channel[3][2] = { {U_plus, U_minus}, {V_plus, V_minus}, {W_plus, W_minus} };
 
 void setup() {
         int pins[7] = {13, U_plus, U_minus, V_plus, V_minus, W_plus, W_minus};
@@ -23,32 +29,41 @@ void setup() {
         }
 }
 
-void OnOff(int pinPlus, int pinMinus, int waitHigh, int waitRecover) {
-  digitalWrite(pinPlus, HIGH); 
-  digitalWrite(pinMinus, HIGH);
-  digitalWrite(13, HIGH);
-  delay(waitHigh);
-  digitalWrite(pinPlus, LOW);
-  digitalWrite(pinMinus, LOW);
-  digitalWrite(13, LOW);
-  delay(waitRecover);
+int waitHigh = 5;
+int waitRecover = 1;
+
+void setChannel(int ch, int level) {
+  int p = channel[ch][plus];
+  int m = channel[ch][minus];
+  if (level == HIGH) { // connect channel to positive voltage
+    digitalWrite(m, LOW);
+    delayMicroseconds(waitRecover);
+    digitalWrite(p, HIGH);
+  }
+  else { // connect channel to ground
+    digitalWrite(p, LOW);
+    delayMicroseconds(waitRecover);
+    digitalWrite(m, HIGH);
+  }
 }
 
 int led = 0;
-int a = 2000;
-int b = 2000;
 
 void loop() {
-  // U-V
-  OnOff(U_plus, W_minus, a, b);
-  
-  // V-W
-  //OnOff(V_plus, W_minus, a, b);
-  
-  // W-U
-  //OnOff(W_plus, U_minus, a, b);
+  setChannel(U, LOW);
+  delay(waitHigh);
+  setChannel(V, HIGH);
+  delay(waitHigh);
+  setChannel(W, LOW);
+  delay(waitHigh);
+  setChannel(U, HIGH);
+  delay(waitHigh);
+  setChannel(V, LOW);
+  delay(waitHigh);
+  setChannel(W, HIGH);
+  delay(waitHigh);
 
   // blink  
-//  led = (led+1) % 1024;
-//  digitalWrite(13, led & 16);
+  led = (led+1) % 1024;
+  digitalWrite(13, led & 16);
 }
